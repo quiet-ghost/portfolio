f6d407f062bab47ed300d25016fa1f8b37099124
K <ksclafani26@pm.me>
2025-04-06 10:17:35 -0400

    idk

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-"use client"
+'use client';
 import { motion } from 'framer-motion';
 import Head from 'next/head';
-
 
 export default function Home() {
   return (
@@ -14,10 +13,10 @@ export default function Home() {
         <motion.h1
           className="text-4xl font-bold text-slate-600 dark:text-zinc-400"
           initial={{ opacity: 0 }}
-          animate={{ 
+          animate={{
             opacity: 1,
             scale: [1, 1.1, 1.1, 1, 1],
-            transition: {duration: 2}
+            transition: { duration: 2 },
           }}
         >
           Welcome to my portfolio!
@@ -29,20 +28,26 @@ export default function Home() {
             initial={{ opacity: 0 }}
             animate={{
               opacity: 1,
-              transition: {duration: 2}
+              transition: { duration: 2 },
             }}
-            >
+          >
             My name is{' '}
-          <span className="text-2xl font-bold opacity-100 text-slate-600 dark:text-blue-400">Kevin Sclafani</span>
-          <p className="mt-4 text-xl text-center">
-          I&apos;m a{' '}
-          <span className="text-2xl font-bold opacity-100 text-slate-600 dark:text-blue-400">Software Engineer</span>{' '}
-          looking to expand my skills and experience. Here is my journey thus far and what I&apos;m looking to accomplish. Some join me in the adventure that is software development.
-        </p>
+            <span className="text-2xl font-bold opacity-100 text-slate-600 dark:text-blue-400">
+              Kevin Sclafani
+            </span>
+            <p className="mt-4 text-xl text-center">
+              I&apos;m a{' '}
+              <span className="text-2xl font-bold opacity-100 text-slate-600 dark:text-blue-400">
+                Software Engineer
+              </span>{' '}
+              looking to expand my skills and experience. Here is my journey
+              thus far and what I&apos;m looking to accomplish. Some join me in
+              the adventure that is software development.
+            </p>
           </motion.h4>
         </div>
-        
       </div>
     </>
   );
 }
+
